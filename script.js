ed3c11efd8efb58de8495dfe40d8cbb7472068e8
Lavisha Saini <167676607+LavishaSaini@users.noreply.github.com>
2025-01-13 09:27:35 +0530

    e commerce website structure

--- script.js
+++ script.js
@@ -1,62 +1,24 @@
-function navAnimation() {
-    var nav = document.querySelector('nav');
-    nav.addEventListener('mouseenter', function () { //gsap has only two things to and from. from is used for starting the work and to is used for ending the work
-        let tl = gsap.timeline() // timeline is used to work in a synchronus flow. First nav-bottom will work then nav h5
+document.querySelector('.more').addEventListener('click',()=>{
+    prompt('Purchased successfully');
+})
 
-        tl.to("#nav-bottom", {
-            height: "23vh"
-        })
-
-        tl.to(".nav-part2 h5", {
-            display: "block"
-        })
-
-        tl.to(".nav-part2 h5 span", {
-            y: 0, // niche se nikal rha hai na isliye 
-            // duration:0.3,
-            stagger: {
-                amount: 0.6 // duration aur stagger ko ek saath karne ki ninja technique
-            }
-        })
-    })
-    nav.addEventListener('mouseleave', function () {
-        let tl = gsap.timeline()
-        tl.to(".nav-part2 h5 span", {
-            y: 25,
-            stagger: {
-                amount: 0.2
-            }
-        })
-        tl.to(".nav-part2 h5", {
-            display: "none",
-            duration: 0.1
-        })
-        tl.to("#nav-bottom", {
-            height: 0,
-            duration: 0.2
-        })
-    })
+function buyDone(){
+    alert('Purchased successfully');
+    
 }
-navAnimation();
 
-function myFunction1() {
-    var popup = document.getElementById("myPopup1");
-    popup.classList.toggle("show");
-  }
-  function myFunction2() {
-    var popup = document.getElementById("myPopup2");
-    popup.classList.toggle("show");
-  }
-  function myFunction3() {
-    var popup = document.getElementById("myPopup3");
-    popup.classList.toggle("show");
-  }
+document.querySelector('.submit').addEventListener('click',()=>{
+    prompt('Congratulations.Your record are sucessfully submit');
+})
 
-document.getElementById("scrollBtn").addEventListener("click", () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-});
-
-function calender(){
-    window.location.href = "eventcalender.html";
+function buysubmit(){
+    alert(' Congratulations.Your record are sucessfully submit');
 }
+document.querySelector('.cart').addEventListener('click',()=>{
+  prompt('ADD to cart succesfully');
+})
+
+function buyAdd(){
+  alert(' Add to cart succesfully');
+}
+
